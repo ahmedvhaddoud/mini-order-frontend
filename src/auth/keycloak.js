aa0419ba53e5479aecad8011210f1700bcae3072
Ahmed Haddoud <ahmedvhaddoud@gmail.com>
2026-07-09 14:30:22 +0200

    change keycloack url

--- src/auth/keycloak.js
+++ src/auth/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js'
 
 const keycloak = new Keycloak({
-    url: 'http://localhost:8080',
+    url: 'https://mini-order-keycloak-production.up.railway.app:9000',
     realm: 'mini-order',
     clientId: 'mini-order-frontend'
 })
