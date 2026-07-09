74212ee68fd5dc0d16bd8b8316e37999c0947189
Ahmed Haddoud <ahmedvhaddoud@gmail.com>
2026-07-09 13:33:51 +0200

    Change back Url deployed in render

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import keycloak from "../auth/keycloak"
 
-const API_URL = "http://localhost:8081/api/orders"
+const API_URL = import.meta.env.VITE_API_URL+"/api/orders"
 
 const getAuthHeader = async () => {
     await keycloak.updateToken(30)
